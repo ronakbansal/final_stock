ce72c0fde1a6d1f4bb39d274eac49bf599ff722f
Learn Ruby <learnruby5@gmail.com>
2012-09-16 14:26:52 +0530

    initial

--- db/migrate/20120909173654_create_portfolios.rb
+++ db/migrate/20120909173654_create_portfolios.rb
@@ -1,11 +1,11 @@
 class CreatePortfolios < ActiveRecord::Migration
   def change
     create_table :portfolios do |t|
-      t.string :user_id
+      t.integer :user_id
       t.string :script_symbol
       t.string :buyorsale
-      t.string :quantity
-      t.string :user_price
+      t.integer :quantity
+      t.integer :user_price
 
       t.timestamps
     end
